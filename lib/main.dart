c78bb082a15fc23eb304e7c85dc6607511fbfd2a
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-03 20:32:52 +0200

    Add words into scrollable list than one at a time

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,24 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+          ),
+        );
+      },
+    );
   }
 }
