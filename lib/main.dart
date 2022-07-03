8cc1af074e0b9f16d58ddf6c942f0d070c38d05c
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-03 20:24:19 +0200

    Add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,3 +24,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
